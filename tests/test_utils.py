8f9d256611307c7b1863e0e5d92d0dd61c00ad86
LiamSennitt <liam.sennitt@gmail.com>
2022-08-15 23:08:40 +0100

    Add test of default rules

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,19 @@
 from unittest import TestCase
 from applocker.policy import AppLockerPolicy
-from applocker.utils import dumps, loads
+from applocker.utils import dump, dumps, load, loads
 
 
 class TestUtils(TestCase):
+    def test_dump(self):
+        pass
+
     def test_dumps(self):
         policy = AppLockerPolicy()
         self.assertEqual(dumps(policy), '<AppLockerPolicy Version="1" />')
+
+    def test_load(self):
+        with open('tests/files/policy.xml', 'r') as file:
+            self.assertIsInstance(load(file), AppLockerPolicy)
 
     def test_loads(self):
         policy = loads('<AppLockerPolicy Version="1" />')
